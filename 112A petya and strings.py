97d7ab15baa0b180ce0b3cc2eb381031e79184a7
rajarshiverma <134830015+rajarshiverma@users.noreply.github.com>
2024-10-04 12:19:04 +0530

    Update 112A petya and strings.py

--- 112A petya and strings.py
+++ 112A petya and strings.py
@@ -1,10 +1,8 @@
-t=int(input())
-words = []
-for i in range(t):
-    w = input().strip()
-    words.append(w)
-for i in range(t):
-    w = words[i]
-    if len(w)>10:
-        w=w[0]+ str(len(w)-2)+w[-1]
-    print(w)
+w1 = input().strip().lower()
+w2 = input().strip().lower()
+if w1>w2:
+    print("1")
+elif w2>w1:
+    print("-1")
+else:
+    print("0")
